9280784378bb923d232578d3f325fa270ed6abc3
アウン <hlumaungphyo@gmail.com>
2024-12-25 14:45:13 +0900

    added alert to the MapView

--- Appetite/Views/MapView.swift
+++ Appetite/Views/MapView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct MapView: View {
+    @StateObject var vm:MapViewModel = MapViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map()
+            .alert(isPresented: $vm.showLocationPermissionAlert) {
+                LocationPermissionAlert()
+            }
+    }
+}
+
+//LocationAlert
+extension MapView{
+    private func LocationPermissionAlert()->Alert{
+        Alert(title: Text("位置情報の使用が制限されています"), primaryButton: .default(Text("設定を開く"), action: {
+            Appetite.LocationPermissionAlert.show()
+        }), secondaryButton: .cancel())
     }
 }
 
