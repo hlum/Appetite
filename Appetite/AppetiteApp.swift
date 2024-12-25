9c0c8c90ac439362d7e22e3799132e2a0f311f6d
アウン <hlumaungphyo@gmail.com>
2024-12-25 11:47:03 +0900

    Initial Commit

---
+++ Appetite/AppetiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiteApp.swift
+//  Appetite
+//
+//  Created by Hlwan Aung Phyo on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
