9a28d72fa7f564020bb1f157ebb0a3c0c771adab
アウン <hlumaungphyo@gmail.com>
2024-12-25 13:30:46 +0900

    Created Custom Errors

---
+++ Appetite/Helpers/CustomErrors.swift
@@ -0,0 +1,13 @@
+//
+//  CustomErrors.swift
+//  Appetite
+//
+//  Created by Hlwan Aung Phyo on 12/25/24.
+//
+
+import Foundation
+
+enum CustomErrors: Error {
+    case NoDataFound
+    case InvalidURL
+}
